005d87294d6d58d555a1d4736c7224bd3f51ec81
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-10 03:09:55 +0300

    Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ChatListPage from './pages/ChatListPage';
 import ChatPage from './pages/ChatPage';
+import UserProfilePage from './pages/UserProfilePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        
         <Route path="/chats" element={<ChatListPage />} />
         <Route path="/chats/:chatId" element={<ChatPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
